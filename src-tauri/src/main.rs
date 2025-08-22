b5064d9b27887be98ada36673d2764c8352b2267
Ilingu <ilingu@duck.com>
2025-08-22 15:00:56 +0200

    app init 2: leptos->svelte because leptos is not suited for tauri even if really interesting +tauri have 90% of their docs with js exemple

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -1,6 +1,11 @@
 // Prevents additional console window on Windows in release, DO NOT REMOVE!!
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
 
-fn main() {
-    fosstoon_lib::run()
+use std::error::Error;
+
+#[tokio::main]
+async fn main() -> Result<(), Box<dyn Error>> {
+    fosstoon_lib::run();
+
+    Ok(())
 }
